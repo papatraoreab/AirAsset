8bec7629826df62c6ba5ed7cc0aa4ffb7c98134a
Papa <papa.traore.external@airbus.com>
2018-11-16 12:27:48 +0100

    update applicationmodel

--- AirAsset/AirAsset/Models/ApplicationModel.cs
+++ AirAsset/AirAsset/Models/ApplicationModel.cs
@@ -33,34 +33,114 @@ namespace AirAsset.Models
             //Init Entrepot 
             EntrepotListe = new Item[]
             {
-                new Item {Value = "1", Label = "Assemblage General"},
+                new Item {Value = "1", Label = "Pieces elementaires metalliques"},
                 new Item {Value = "2", Label = "Assemblage S/E"},
-                new Item {Value = "3", Label = "Finition"},
-                new Item {Value = "4", Label = "Magasin Consommables (W20)"},
-                new Item {Value = "5", Label = "Magasin Equipement (W15G)"},
-                new Item {Value = "6", Label = "Magasin Matieres Composites (M0)"},
-                new Item {Value = "7", Label = "Magasin Matieres Dangereuses (032)"},
-                new Item {Value = "8", Label = "Magasin Matieres Metalliques (F07 + Parc)"},
-                new Item {Value = "9", Label = "Magasin Outils Coupants (C12)"},
-                new Item {Value = "10", Label = "Market Place Grandes Pieces (I05)"},
-                new Item {Value = "11", Label = "Market Place Petites Pieces (W15D)"},
-                new Item {Value = "12", Label = "Pieces Elementaires Composites"},
-                new Item {Value = "13", Label = "Pieces Elementaires Metalliques"},
-                new Item {Value = "14", Label = "Autres"}
+                new Item {Value = "3", Label = "Assemblage general"},
+                new Item {Value = "4", Label = "Autre"},
+                new Item {Value = "5", Label = "Pieces Elementaires Composites"},
+                new Item {Value = "6", Label = "Magasin Outils Coupants (C12)"},
+                new Item {Value = "7", Label = "Magasin Matieres Metalliques (F07 + Parc)"},
+                new Item {Value = "8", Label = "Magasin matieres composites (M0)"},
+                new Item {Value = "9", Label = "Magasin matieres dangereuses (O32)"},
+                new Item {Value = "10", Label = "Magasin pieces elementaires (O37)"},
+                new Item {Value = "11", Label = "Magasin equipements (W15G)"},
+                new Item {Value = "12", Label = "Magasin consommables (W20)"},
+                new Item {Value = "13", Label = "Market place grandes pieces (I05)"},
+                new Item {Value = "14", Label = "Market place petites pieces (W15D)"},
+                new Item {Value = "15", Label = "Stockage produits finis (ZB12)"},
+                new Item {Value = "16", Label = "Stockage produits finis (ZB34)"},
+                new Item {Value = "17", Label = "Stockage produits finis (ZC22)"}
             };
 
 
             //Init Localisation 
             LocalisationListe = new Item[]
             {
-                new Item {Value = "1", Label = "A6"},
-                new Item {Value = "2", Label = "A15"},
-                new Item {Value = "3", Label = "C1"},
-                new Item {Value = "4", Label = "C4"},
-                new Item {Value = "5", Label = "Flower"},
-                new Item {Value = "6", Label = "ZB12"}
+                new Item {Value = "1", Label = "A07"},
+                new Item {Value = "2", Label = "A11"},
+                new Item {Value = "3", Label = "A15"},
+                new Item {Value = "4", Label = "B28"},
+                new Item {Value = "5", Label = "C12"},
+                new Item {Value = "6", Label = "E03"},
+                new Item {Value = "7", Label = "F07"},
+                new Item {Value = "8", Label = "G03"},
+                new Item {Value = "9", Label = "G19"},
+                new Item {Value = "10", Label = "H36"},
+                new Item {Value = "11", Label = "I05"},
+                new Item {Value = "12", Label = "I07"},
+                new Item {Value = "13", Label = "I31"},
+                new Item {Value = "14", Label = "J00"},
+                new Item {Value = "15", Label = "K01"},
+                new Item {Value = "16", Label = "K06"},
+                new Item {Value = "17", Label = "K101"},
+                new Item {Value = "18", Label = "K15"},
+                new Item {Value = "19", Label = "K23"},
+                new Item {Value = "20", Label = "K33"},
+                new Item {Value = "21", Label = "L09"},
+                new Item {Value = "22", Label = "L39"},
+                new Item {Value = "23", Label = "M0"},
+                new Item {Value = "24", Label = "M03"},
+                new Item {Value = "25", Label = "N04"},
+                new Item {Value = "26", Label = "N37"},
+                new Item {Value = "27", Label = "N41"},
+                new Item {Value = "28", Label = "O01"},
+                new Item {Value = "29", Label = "O101"},
+                new Item {Value = "30", Label = "O32"},
+                new Item {Value = "31", Label = "O34"},
+                new Item {Value = "32", Label = "O37"},
+                new Item {Value = "33", Label = "O7"},
+                new Item {Value = "34", Label = "P05"},
+                new Item {Value = "35", Label = "P20"},
+                new Item {Value = "36", Label = "P34"},
+                new Item {Value = "37", Label = "Q03"},
+                new Item {Value = "38", Label = "Q04"},
+                new Item {Value = "39", Label = "Q06"},
+                new Item {Value = "40", Label = "R14"},
+                new Item {Value = "41", Label = "S22"},
+                new Item {Value = "42", Label = "S26"},
+                new Item {Value = "43", Label = "S30"},
+                new Item {Value = "44", Label = "U12"},
+                new Item {Value = "45", Label = "U34"},
+                new Item {Value = "46", Label = "U37"},
+                new Item {Value = "47", Label = "V13"},
+                new Item {Value = "48", Label = "V18"},
+                new Item {Value = "49", Label = "V24"},
+                new Item {Value = "50", Label = "W11"},
+                new Item {Value = "51", Label = "W15"},
+                new Item {Value = "52", Label = "W29"},
+                new Item {Value = "53", Label = "W32"},
+                new Item {Value = "54", Label = "W6"},
+                new Item {Value = "55", Label = "X13"},
+                new Item {Value = "56", Label = "Y04"},
+                new Item {Value = "57", Label = "Y10"},
+                new Item {Value = "58", Label = "Y13"},
+                new Item {Value = "59", Label = "Y26"},
+                new Item {Value = "60", Label = "Y6"},
+                new Item {Value = "61", Label = "ZA35"},
+                new Item {Value = "62", Label = "ZB06"},
+                new Item {Value = "63", Label = "ZB12"},
+                new Item {Value = "64", Label = "ZB19"},
+                new Item {Value = "65", Label = "ZB34"},
+                new Item {Value = "66", Label = "ZC20"},
+                new Item {Value = "67", Label = "ZC22"},
+                new Item {Value = "68", Label = "ZC24"},
+                new Item {Value = "69", Label = "ZD30"},
+                new Item {Value = "70", Label = "ZG23"},
+                new Item {Value = "71", Label = "ZH38"},
+                new Item {Value = "72", Label = "ZI27"},
+                new Item {Value = "73", Label = "ZK30"},
+                new Item {Value = "74", Label = "ZL46"},
+                new Item {Value = "75", Label = "ZM40"},
+                new Item {Value = "76", Label = "ZO49"},
+                new Item {Value = "77", Label = "ZP46"},
+                new Item {Value = "78", Label = "ZP53"},
+                new Item {Value = "79", Label = "ZP56"},
+                new Item {Value = "80", Label = "ZQ44"},
+                new Item {Value = "81", Label = "ZR54"},
+                new Item {Value = "82", Label = "ZU49"}
             };
-
+            
+            
             /*
             //Init Fournisseur
             FournisseurListe = new Item[]
@@ -86,7 +166,8 @@ namespace AirAsset.Models
                 new Item {Value = "3", Label = "A350"},
                 new Item {Value = "4", Label = "A380"},
                 new Item {Value = "5", Label = "A400M"},
-                new Item {Value = "6", Label = "ATR"}
+                new Item {Value = "6", Label = "ATR"},
+                new Item {Value = "7", Label = "Logistics"}
             };
 
 
@@ -94,12 +175,13 @@ namespace AirAsset.Models
             SecteurListe = new Item[]
             {
                 new Item {Value = "1", Label = "Air Inlet"},
-                new Item {Value = "2", Label = "CWB"},
-                new Item {Value = "3", Label = "Fin"},
-                new Item {Value = "4", Label = "Keelbeam"},
-                new Item {Value = "5", Label = "Logistics"},
-                new Item {Value = "6", Label = "Radome"},
-                new Item {Value = "7", Label = "Rudder"}
+                new Item {Value = "2", Label = "Aileron"},
+                new Item {Value = "3", Label = "CWB"},
+                new Item {Value = "4", Label = "Fin"},
+                new Item {Value = "5", Label = "Keelbeam"},
+                new Item {Value = "6", Label = "Logistics"},
+                new Item {Value = "7", Label = "Radome"},
+                new Item {Value = "8", Label = "Rudder"}
 
             };
 
@@ -126,18 +208,19 @@ namespace AirAsset.Models
             TypeListe = new Item[]
             {
                 new Item {Value = "1", Label = "Bache"},
-                new Item {Value = "2", Label = "Caisse"},
-                new Item {Value = "3", Label = "Chariot"},
-                new Item {Value = "4", Label = "Congélateur"},
-                new Item {Value = "5", Label = "Etagere"},
-                new Item {Value = "6", Label = "Ferrure"},
-                new Item {Value = "7", Label = "Micro-Ondes"},
-                new Item {Value = "8", Label = "Palettier"},
-                new Item {Value = "9", Label = "Palonnier"},
-                new Item {Value = "10", Label = "Raletier"},
-                new Item {Value = "11", Label = "Roll"},
-                new Item {Value = "12", Label = "Supply Point"},
-                new Item {Value = "13", Label = "Traine"}
+                new Item {Value = "2", Label = "Palonnier"},
+                new Item {Value = "3", Label = "Ferrure"},
+                new Item {Value = "4", Label = "Supply point"},
+                new Item {Value = "5", Label = "Congelateur"},
+                new Item {Value = "6", Label = "Micro-ondes"},
+                new Item {Value = "7", Label = "Palettier"},
+                new Item {Value = "8", Label = "Etagere"},
+                new Item {Value = "9", Label = "Raletier"},
+                new Item {Value = "10", Label = "Cantilever"},
+                new Item {Value = "11", Label = "Jig Transport"},
+                new Item {Value = "12", Label = "Rack dynamique"},
+                new Item {Value = "13", Label = "Remorque"},
+                new Item {Value = "14", Label = "Roll"}
             };
         }
     }
